66c87b098e50823f145361cacc3364cd656ae248
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-10 14:43:34 +0530

    Brand New

--- demo.js
+++ demo.js
@@ -4,9 +4,7 @@ let box=document.getElementById('main-header');
 box.style.border='solid 3px black';
 
 let list=document.getElementsByClassName('list-group-item');
+list[2].style.background='green'
 for (let i=0;i<list.length;i++){
-    list[i].style.background='#f4f4f4'
+    list[i].style.fontWeight='bold'
 }
-list[1].textContent='Hello Sir'
-list[1].style.fontWeight='bold'
-list[1].style.color='green'
