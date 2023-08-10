88f1ba22d779130fc084e2ff92c114ef48a540b4
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-10 14:49:42 +0530

    By tag and class

--- demo.js
+++ demo.js
@@ -3,8 +3,13 @@ title.textContent='Modified Hurray!!';
 let box=document.getElementById('main-header');
 box.style.border='solid 3px black';
 
+
+let tag=document.getElementsByTagName('li')
+for (let i=0;i<tag.length;i++){
+    tag[i].style.backgroundColor='red'
+}
 let list=document.getElementsByClassName('list-group-item');
-list[2].style.background='green'
 for (let i=0;i<list.length;i++){
     list[i].style.fontWeight='bold'
+    list[i].style.backgroundColor='#ccc'
 }
