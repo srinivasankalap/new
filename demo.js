5f7f7fae054079c968d4e6494e7bdb2c96279bc5
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-10 14:39:16 +0530

    Recent Commit

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+let title=document.getElementById('header-title');
+title.textContent='Modified Hurray!!';
+let box=document.getElementById('main-header');
+box.style.border='solid 3px black';
+
+let list=document.getElementsByClassName('list-group-item');
+for (let i=0;i<list.length;i++){
+    list[i].style.background='#f4f4f4'
+}
+list[1].textContent='Hello Sir'
+list[1].style.fontWeight='bold'
+list[1].style.color='green'
