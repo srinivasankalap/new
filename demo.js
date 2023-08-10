a6e025bb485a76361d14552b90acdb322db3c42b
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-10 16:42:37 +0530

    Buttons

--- demo.js
+++ demo.js
@@ -1,32 +1,44 @@
-let list=document.querySelector('#items')
-console.log(list.parentElement)
-list.parentElement.style.backgroundColor='#ccc'
+let form=document.getElementById('addForm');
+let itemList=document.getElementById('items');
+let filter=document.getElementById('filter');
 
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterItems);
 
-console.log(list.children)
+function addItem(e){
+    e.preventDefault();
 
-console.log(list.firstElementChild)
-list.firstElementChild.textContent='Hello Buddy'
+    let newItem=document.getElementById('item').value;
+    let li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    let deleteBtn=document.createElement('button');
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
 
-console.log(list.lastElementChild)
-list.lastElementChild.textContent="Fool"
+}
 
-console.log(list.previousElementSibling)
-list.previousElementSibling.style.color='yellow'
+function removeItem(e){
+    if (e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+        }
+    }
+}
 
-
-let newdiv=document.createElement('div')
-newdiv.className='new'
-newdiv.id='newdiv'
-newdiv.setAttribute('title','Hello world')
-console.log(newdiv)
-
-let value=document.createTextNode('Hello World')
-newdiv.appendChild(value)
-console.log(newdiv)
-
-
-let container=document.querySelector('header .container');
-let x=document.querySelector('header h1')
-
-container.insertBefore(newdiv,x)
+function filterItems(e){
+    let text=e.target.value.toLowerCase();
+    let items=itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        let itemName=item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display='block';
+        }else{
+            item.style.display='none';
+        }
+    });
+}
