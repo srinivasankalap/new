fa1425eed72f93fa2e26d7667282f7903245a978
srinivasankalap <cnu.ankalap@gmail.com>
2023-08-11 11:03:26 +0530

    Removing from screen and LocalStorage

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 let form=document.getElementById('my-form');
 form.addEventListener('submit',store);
 let itemList=document.getElementById('items')
+itemList.addEventListener('click',removeItem);
 function store(e){
     e.preventDefault()
     let temp=document.getElementById('name').value;
@@ -16,5 +17,20 @@ function store(e){
     let li=document.createElement('li');
     li.className='item';
     li.appendChild(document.createTextNode(temp+' - '+email));
+    let deleteBtn=document.createElement('button');
+    deleteBtn.className='btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    li.appendChild(deleteBtn);
     itemList.appendChild(li);
 };
+
+
+function removeItem(e){
+    
+    let email=document.getElementById('email').value;
+    if (e.target.classList.contains('delete')){
+            var li=e.target.parentElement;
+            itemList.removeChild(li)
+            localStorage.removeItem(email)
+    }
+}
